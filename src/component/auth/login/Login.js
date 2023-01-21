e6365a65a90056ae770ae483a7b19c6b175bf26c
lizajohnson431 <112683974+lizajohnson431@users.noreply.github.com>
2023-01-21 17:58:54 +0530

    RegisterPageUi

--- src/component/auth/login/Login.js
+++ src/component/auth/login/Login.js
@@ -1,8 +1,13 @@
 import React from 'react'
 import { Row, Col, Container, Form, Button } from "react-bootstrap";
+import { useNavigate } from 'react-router-dom';
 
 import './Login.scss'
 const Login = () => {
+  let navigate = useNavigate();
+  const RouteChange = (path) => {
+    navigate(path);
+  };
   return (
     <React.Fragment>
       <Row className='login-page'>
@@ -23,18 +28,18 @@ const Login = () => {
                   <Form.Control type="password" placeholder="Password" className='mt-2' />
                 </Form.Group>
                 <div className='f-pass'>
-                  <span>forgot password?</span>
+                  forgot password?
                 </div>
                 <Button variant="dark" type="submit" className='login-button'>
                   Login
                 </Button>
-                <Button variant="light" type="submit" className='register-button'>
+                <Button variant="light" type="submit" className='register-button' onClick={() => RouteChange("/register")}>
                   Register
                 </Button>
               </Form>
             </Container>
           </div>
-        </Col> 
+        </Col>
       </Row>
     </React.Fragment>
   )
--- src/component/auth/register/Register.js
+++ src/component/auth/register/Register.js
@@ -1,8 +1,72 @@
 import React from 'react'
+import { Col, Form, Row, Button, FormControl, Container, } from 'react-bootstrap'
+import { Link } from 'react-router-dom'
+import "./Register.scss"
 
 const Register = () => {
   return (
-    <div>Register</div>
+    <Row className='main-div'>
+      <Col lg={6} sm={6} md={6} className='left-part d-flex justify-content-center align-items-center'>
+        <h1>SMART PHYSIO</h1>
+      </Col>
+      <Col lg={6} sm={6} md={6} className='right-part  d-flex justify-content-center align-items-center'>
+        <Container>
+          <Form>
+            <div className='header'>
+              <h1>Register</h1>
+            </div>
+            <div className='register'>
+
+                <Form.Group className="mb-3" controlId="formBasicEmail">
+                  <label>Clinic Name</label>
+                  <FormControl className='mt-2' type="text" placeholder="Clinic Name" />
+                </Form.Group>
+            
+
+                <Form.Group className="mb-3" controlId="formBasicEmail">
+                  <label>Doctor Name</label>
+                  <FormControl  className='mt-2' type="text" placeholder="Doctor Name" />
+                </Form.Group>
+
+                <Form.Group className="mb-3" controlId="formBasicEmail">
+                  <label>Email address</label>
+                  <FormControl className='mt-2'type="email" placeholder="Enter Your Email" />
+                </Form.Group>
+
+                <Form.Group className="mb-3" controlId="formBasicEmail">
+                  <label>Contact No</label>
+                  <FormControl className='mt-2'placeholder="Contact No" />
+                </Form.Group>
+
+              <Row>
+                <Col lg={6} sm={6} md={3}>
+                  <Form.Group className="mb-3" controlId="formBasicPassword">
+                    <label>Password</label>
+                    <FormControl className='mt-2' type="password" placeholder="Password" />
+                  </Form.Group>
+                </Col>
+
+                <Col lg={6} sm={6} md={3}>
+                  <Form.Group className="mb-3" controlId="formBasicPassword">
+                    <label>Confirm Password</label>
+                    <FormControl className='mt-2' type="password" placeholder="Confirm Password" />
+                  </Form.Group>
+                </Col>
+              </Row>
+
+              <Button variant="dark" type="submit">
+                Register
+              </Button>
+              <div className="d-flex justify-content-center">
+                <span className='mt-3'>
+                  Already have an account?<Link to="/"> Log in</Link>
+                </span>
+              </div>
+            </div>
+          </Form>
+        </Container>
+      </Col>
+    </Row>
   )
 }
 
