5b747f36c6419b6dd7501cd24c1fa3ff1c06e1fc
Parth Gondaliya <pgondaliya303@gmail.com>
2023-01-25 15:14:00 +0530

    - Icon Changes

--- src/component/sidebar/Sidemenu/Sidemenu.jsx
+++ src/component/sidebar/Sidemenu/Sidemenu.jsx
@@ -2,15 +2,15 @@ export const MENUITEMS = [
     {
         Items: [
             { path: `/dashboard`, icon: 'fe fe-home', type: 'link', active: false, selected: false, title: 'Dashboard' },
-            { path: `/patients`, icon: 'fe fe-home', type: 'link', active: false, selected: false, title: 'Patients' },
-            { path: `/chief-complaint`, icon: 'fe fe-home', type: 'link', active: false, selected: false, title: 'Chief-complaint' },
-            { path: `/patient-history`, icon: 'fe fe-home', type: 'link', active: false, selected: false, title: 'Patient History' },
-            { path: `/pain-assesment`, icon: 'fe fe-home', type: 'link', active: false, selected: false, title: 'Pain Assesment' },
-            { path: `/examination`, icon: 'fe fe-home', type: 'link', active: false, selected: false, title: 'Examination' },
-            { path: `/special-test`, icon: 'fe fe-home', type: 'link', active: false, selected: false, title: 'Special Test' },
-            { path: `/investigation`, icon: 'fe fe-home', type: 'link', active: false, selected: false, title: 'Investigation' },
-            { path: `/schedule-appoint`, icon: 'fe fe-home', type: 'link', active: false, selected: false, title: 'Schedule Appoint' },
-            { path: `/notification`, icon: 'fe fe-home', type: 'link', active: false, selected: false, title: 'Notification' },
+            { path: `/patients`, icon: 'fe fe-users', type: 'link', active: false, selected: false, title: 'Patients' },
+            { path: `/chief-complaint`, icon: 'fe fe-mail', type: 'link', active: false, selected: false, title: 'Chief-complaint' },
+            { path: `/patient-history`, icon: 'fe fe-clock', type: 'link', active: false, selected: false, title: 'Patient History' },
+            { path: `/pain-assesment`, icon: 'fe fe-activity', type: 'link', active: false, selected: false, title: 'Pain Assesment' },
+            { path: `/examination`, icon: 'fe fe-book', type: 'link', active: false, selected: false, title: 'Examination' },
+            { path: `/special-test`, icon: 'fe fe-codepen', type: 'link', active: false, selected: false, title: 'Special Test' },
+            { path: `/investigation`, icon: 'fe fe-map', type: 'link', active: false, selected: false, title: 'Investigation' },
+            { path: `/schedule-appoint`, icon: 'fe fe-layout', type: 'link', active: false, selected: false, title: 'Schedule Appoint' },
+            { path: `/notification`, icon: 'fe fe-bell', type: 'link', active: false, selected: false, title: 'Notification' },
         ]
     },
 ]
